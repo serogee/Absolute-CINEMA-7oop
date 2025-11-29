d5b09852343cad659291ae83216200bf64ada7b9
serosero <sggblank@gmail.com>
2025-11-29 23:36:00 +0800

    🤓 Moved ExperienceType and removed unnecessary files

---
+++ src/screening/utils/ExperienceType.java
@@ -0,0 +1,18 @@
+package screening.utils;
+
+public enum ExperienceType {
+    STANDARD_2D("Standard 2D"),
+    STANDARD_3D("Standard 3D"),
+    IMAX("IMAX");
+
+    private final String displayName;
+
+    private ExperienceType(String displayName) {
+        this.displayName = displayName;
+    }
+
+    @Override
+    public String toString() {
+        return this.displayName;
+    }
+}
